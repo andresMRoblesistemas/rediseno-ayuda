7fb9ec4913c49b8ee41a37434e2f1fad319948ce
Andres <106349694+andresMRoblesistemas@users.noreply.github.com>
2022-10-25 17:24:52 -0300

    se incorpora js de galeria

--- assets/js/imagePopup.js
+++ assets/js/imagePopup.js
@@ -1,25 +1,8 @@
-$(document).ready(function() {
-
-    // required elements
-    var imgPopup = $('.img-popup');
-    var imgCont  = $('.grid__img-holder');
-    var popupImage = $('.img-popup img');
-    var closeBtn = $('.close-btn');
-  
-    // handle events
-    imgCont.on('click', function() {
-      var img_src = $(this).children('img').attr('src');
-      imgPopup.children('img').attr('src', img_src);
-      imgPopup.addClass('opened');
+$(document).ready(function(){
+    $("#textToggler").click(function(){
+      $(".toggleText").toggle();
     });
-  
-    $(imgPopup, closeBtn).on('click', function() {
-      imgPopup.removeClass('opened');
-      imgPopup.children('img').attr('src', '');
-    });
-  
-    popupImage.on('click', function(e) {
-      e.stopPropagation();
-    });
-    
   });
+  function toggleImage() {
+    $(".hiddenclickimg").toggle();
+  };
