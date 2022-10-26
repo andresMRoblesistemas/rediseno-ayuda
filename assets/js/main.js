b9c859d54a81072df22bcee9fe4c32d69a1766c0
Andres <106349694+andresMRoblesistemas@users.noreply.github.com>
2022-10-26 10:11:03 -0300

    todas las imagenes

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+(function () {
+
+  window.onscroll = function () {
+    const header_navbar = document.querySelector(".navbar-area");
+    const sticky = header_navbar.offsetTop;
+    const logo = document.querySelector(".navbar-brand img");
+
+    if (window.pageYOffset > sticky) {
+      header_navbar.classList.add("sticky");
+      logo.src = "../assets/images/logoMDpalabras.svg";
+    } else {
+      header_navbar.classList.remove("sticky");
+      
+    }
+
+
+  };
+
+  let navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  document.querySelectorAll(".page-scroll").forEach((e) =>
+    e.addEventListener("click", () => {
+      navbarToggler.classList.remove("active");
+      navbarCollapse.classList.remove("show");
+    })
+  );
+  navbarToggler.addEventListener("click", function () {
+    navbarToggler.classList.toggle("active");
+  });
+
+  // WOW active
+  new WOW().init();
+})();
