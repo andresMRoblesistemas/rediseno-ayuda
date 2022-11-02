6fa9e9b1de7c79557a7aad0fd34d74119854b661
Andres <106349694+andresMRoblesistemas@users.noreply.github.com>
2022-11-02 09:59:41 -0300

    seccion luminarias

---
+++ assets/js/redirect.js
@@ -0,0 +1,11 @@
+const params = new URLSearchParams(document.location.search);
+switch(params.get('destino')) {
+case 'gps':
+    window.location.replace('https://ayuda.cdmisiones.net.ar/vistas/reclamos/luminarias/tema10.html#01d')
+    break;
+case 'foto':
+    window.location.replace('https://ayuda.cdmisiones.net.ar/vistas/reclamos/luminarias/tema10.html#02d')
+    break;
+default:
+//window.location.replace('https://ayuda.cdmisiones.net.ar/')
+}
